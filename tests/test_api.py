9af419237208069536e1bbbb0111b0f95de597c7
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-02-28 15:11:21 +0000

    ajout de fonctionnalite de tests

--- tests/test_api.py
+++ tests/test_api.py
@@ -1,1 +1,11 @@
-# tests/test_api.py
+from fastapi.testclient import TestClient
+from app.main import app
+
+# On crée un faux client web pour tester notre API
+client = TestClient(app)
+
+def test_health_check():
+    """Vérifie que l'API démarre correctement et renvoie le statut OK"""
+    response = client.get("/health")
+    assert response.status_code == 200
+    assert response.json() == {"status": "ok", "service": "CliniQ API", "version": "1.0.0"}
