a0aeb675fe062829742eb96fc5b416942ae3a624
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-02-28 15:07:06 +0000

    modififcation dans schemas query

--- app/schemas/query_schema.py
+++ app/schemas/query_schema.py
@@ -1,1 +1,11 @@
-# app/schemas/query_schema.py
+from pydantic import BaseModel
+from typing import List
+
+class QueryRequest(BaseModel):
+    question: str
+
+class QueryResponse(BaseModel):
+    id: int
+    question: str
+    reponse: str
+    sources: List[str] 
