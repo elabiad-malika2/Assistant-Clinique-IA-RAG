471d79529992892548a52b4e0da113c25a8b99a8
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-02-28 15:11:03 +0000

    ajout de fonctionalite de retriever

--- rag/retriever.py
+++ rag/retriever.py
@@ -1,1 +1,12 @@
-# rag/retriever.py
+from typing import List
+from rag.embedding import embed_text
+from rag.vector_store import search_similar_in_chroma
+
+def retrieve_documents(query: str) -> List[str]:
+    """Étape 1 : Va chercher les 5 passages les plus proches de la question."""
+    query_emb = embed_text(query)
+    results = search_similar_in_chroma(query_emb, top_k=5)
+    
+    # On récupère juste la liste des textes trouvés
+    documents = results.get("documents", [[]])[0]
+    return documents
