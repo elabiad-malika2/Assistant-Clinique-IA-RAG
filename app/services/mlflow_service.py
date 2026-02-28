59e473e91436034f2eac08b4f332de9bd0062f5b
elabiad-malika2 <elabiadmalika64@gmail.com>
2026-02-28 15:08:15 +0000

    ajout mlflow et rag services

--- app/services/mlflow_service.py
+++ app/services/mlflow_service.py
@@ -1,1 +1,47 @@
 # app/services/mlflow_service.py
+
+import mlflow
+import os
+
+mlflow.set_tracking_uri(os.getenv("MLFLOW_TRACKING_URI", "http://mlflow:5000"))
+mlflow.set_experiment("CliniQ_RAG_Production")
+
+def log_to_mlflow(question: str, response: str, contexts: list, metrics: dict):
+    """
+    Enregistre absolument TOUT dans le tableau de bord MLflow.
+    """
+    print("📝 Sauvegarde dans MLflow...")
+    
+    with mlflow.start_run():
+        
+        # 1. Logger les paramètres du RAG
+        mlflow.log_params({
+            "chunk_size": 1200,
+            "chunk_overlap": 100,
+            "chunk_strategy": "MarkdownHeaderTextSplitter",
+            "embedding_model": "dangvantuan/sentence-camembert-base",
+            "embedding_dim": 768,
+            "retrieval_algo": "cosine",
+            "retrieval_k": 5,
+            "reranking_model": "ms-marco-MiniLM-L-6-v2"
+        })
+        
+        # 2. Logger les paramètres du LLM (Gemini)
+        mlflow.log_params({
+            "llm_model": "gemini-2.5-flash-lite",
+            "temperature": 0.0,
+            "top_p": 0.9,
+            "top_k": 40,
+            "max_tokens": 1024,
+            "prompt_template": "CliniQ_Strict_Medical_Prompt"
+        })
+        
+        # 3. Logger les réponses et contextes (dans des petits fichiers textes lisibles sur l'interface)
+        mlflow.log_text(question, "logs/question.txt")
+        mlflow.log_text(response, "logs/response.txt")
+        mlflow.log_text("\n\n---\n\n".join(contexts), "logs/contexts.txt")
+        
+        # 4. Logger les 4 notes données par DeepEval
+        mlflow.log_metrics(metrics)
+        
+    print(" Données visibles sur http://localhost:5000 !")
--- app/services/rag_service.py
+++ app/services/rag_service.py
@@ -1,1 +1,50 @@
-# app/services/rag_service.py
+from sqlalchemy.orm import Session
+from app.models.query import Query
+from rag.rag_pipeline import ask_clinical_assistant
+from mlops.deepeval_metrics import evaluate_rag_response
+from app.services.mlflow_service import log_to_mlflow
+
+from monitoring.prometheus_metrics import (
+    RAG_REQUESTS_TOTAL, 
+    RAG_ERRORS_TOTAL, 
+    RAG_ANSWER_RELEVANCE, 
+    RAG_FAITHFULNESS
+)
+
+def process_and_save_query(db: Session, user_id: int, question: str):
+    """Fait appel à l'IA et sauvegarde le résultat dans PostgreSQL."""
+
+    # +1 au compteur des requêtes posées !
+    RAG_REQUESTS_TOTAL.inc()
+    
+    try:
+        # 1. On appelle notre IA (Le Chef d'Orchestre)
+        reponse_ia, sources = ask_clinical_assistant(question)
+        
+        # 2. On sauvegarde dans la table 'queries' de la base de données
+        nouvelle_requete = Query(
+            query=question,
+            reponse=reponse_ia,
+            user_id=user_id # On lie la question au médecin connecté !
+        )
+        db.add(nouvelle_requete)
+        db.commit()
+        db.refresh(nouvelle_requete)
+
+        # 3. MLOPS : On note la réponse avec DeepEval
+        notes_ia = evaluate_rag_response(question, reponse_ia, sources)
+        
+        # 4. MLOPS : On envoie tout au tableau de bord MLflow
+        log_to_mlflow(question, reponse_ia, sources, notes_ia)
+
+        # 5. MONITORING : On met à jour les jauges de qualité pour Prometheus !
+        RAG_ANSWER_RELEVANCE.set(notes_ia.get("answer_relevance", 0.0))
+        RAG_FAITHFULNESS.set(notes_ia.get("faithfulness", 0.0))
+
+    except Exception as e:
+        # +1 au compteur des erreurs si l'IA ou la DB plante !
+        RAG_ERRORS_TOTAL.inc()
+        print(f" Erreur critique du RAG : {e}")
+        raise e
+        
+    return nouvelle_requete, sources
